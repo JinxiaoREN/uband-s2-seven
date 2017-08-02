230b9f9a5a738027cb4338a35347a08ee99751f7
aaaAcie <913587892@qq.com>
2017-08-02 17:50:30 +0800

    这次对了=，=

--- run.py
+++ run.py
@@ -7,5 +7,10 @@ def hello_world():
     # return 'Hello World!'
     return render_template('SKY.html')
 
+@app.route('/a13376')
+def a13376():
+    # return 'Hello World!'
+    return render_template('a13376.html')
+
 if __name__ == '__main__':
     app.run()
