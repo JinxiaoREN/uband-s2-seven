e6857625d5c35abc4b72b1bd1a3751083414572d
sy <1293244060@qq.com>
2017-08-04 12:42:48 +0800

    no message

--- run.py
+++ run.py
@@ -7,5 +7,9 @@ def hello_world():
     # return 'Hello World!'
     return render_template('SKY.html')
 
+@app.route('/<string:student_number>/detailes')
+def details(student_number):
+	return render_template(student_number + '.html')
+
 if __name__ == '__main__':
     app.run()
