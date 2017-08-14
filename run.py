8e57ac70fd6a2cafabf9a3232f9af2b456c6c3eb
sy <1293244060@qq.com>
2017-08-14 22:51:13 +0800

    no message

--- run.py
+++ run.py
@@ -3,13 +3,18 @@ from flask import render_template
 import json
 app = Flask(__name__)
 
-
+def read_json_file(filepath):
+	file=open(filepath,"r+")
+	file_text=file.read()
+	data=json.loads(file_text)
+	return data
 
 @app.route('/')
 def index():
-	file=open("./static/data/index.json","r+")
-	file_text=file.read()
-	data=json.loads(file_text)
+	# file=open("./static/data/index.json","r+")
+	# file_text=file.read()
+	# data=json.loads(file_text)
+	data=read_json_file('./static/data/index.json')
 	return render_template('index.html',data=data)
 
 # @app.route('/helloworld')
@@ -22,9 +27,15 @@ def index():
 #     print file_text['name']
 #     return render_template('SKY.html',data=file_text)
 
-# @app.route('/<string:student_number>/detailes')
-# def details(student_number):
-# 	return render_template(student_number + '.html')
+@app.route('/details/<string:student_number>')
+def details(student_number):
+	data=read_json_file('./static/data/index.json')
+	user_data={}
+	for item in data:
+		if student_number==item['student_number']:
+			user_data=item
+			break
+	return render_template('details.html',data=user_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
